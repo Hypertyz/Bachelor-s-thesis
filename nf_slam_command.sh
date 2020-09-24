1edc468dda9d647fafdae32b7b54cbc2147babde
drowsygoat <lecka48@liu.se>
2020-09-24 17:46:58 +0200

    update

--- nf_slam_command.sh
+++ nf_slam_command.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-# was producing an error
+# Run as follows: ./nf_slam_command.sh [run names]
 
 nextflow run nf-core/slamseq 	--input RTS0_nf_input.tsv \
-								-name test_run_lk \
-							 	-profile test \
+								-name $1 \
+							 	-profile docker,test \
 								--fasta GRCm38.primary_assembly.genome.fa \
 								--bed 3UTRs.bed \
 								--mapping 3UTRs.bed \
-						  		--igenomes_ignore=true \
-								--trim5 0 \
-								--multimappers=true \
-							    --quantseq=true \
-								--endtoend=false \
+								--trim5 2 \
+								--polyA 8 \
+								--multimappers false \
+							    --quantseq true \
+								--endtoend true \
 								--base_quality 20 \
-								--read_length \
 								--pvalue 0.05 \
-								--skip_trimming=true \
-								--skip_deseq2=false \
+								--readlength 300 \
+								--skip_deseq2 true \
 								--email lecka48@liu.se \
 								--email_on_fail lecka48@liu.se
 								
